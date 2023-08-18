625ef91983f47624d15bc283d066df6b473ce364
Bakhita87 <bakhitaotieno@gmail.com>
2023-08-19 00:59:51 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a= 0; b= 1
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) <= length:
+        new_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(new_number)
+
+    print(fibonacci_sequence)
+
+
+print_fibonacci(9) 
